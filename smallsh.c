08db7911776d274feee3af5dfb47660bf0b6de65
Alessandro Vaprio <alessandro.vaprio@elvispos.com>
2019-08-15 18:28:29 +0200

    Unused the env for background process

--- smallsh.c
+++ smallsh.c
@@ -3,16 +3,19 @@
 #include <string.h>
 #include <signal.h>
 #include <unistd.h>
+#include <stdlib.h>
+char *strremove(char *str, char *sub);
+char *convertIntToString(int n);
 
-char *prompt = "";
-pid_t pid;
-struct sigaction sa;
-int exitstat, ret;
+  void insertPid(int pid);
+  char *prompt = "";
+  pid_t pid;
+  struct sigaction sa;
+  int exitstat, ret;
 
+  void sig_handler(int signo)
+  {
 
-void sig_handler(int signo)
-{
-  
     if (signo == SIGINT)
     {
       //kill(pid, SIGTERM);
@@ -22,77 +25,110 @@ void sig_handler(int signo)
       //kill(pid, SIGINT);
       //return EXIT_FAILURE;
     }
-    
-}
-char * removePid(int pid){
-  //controllo che la variabile contenga pid+,
-  if (strstr(getenv("BPID"), srtcat(convertIntToString(pid),",")) != NULL)
+  }
+  void removePid(int pid)
   {
-    return strremove(getenv("BPID"), srtcat(convertIntToString(pid), ","));
-  }
-  //controllo che la variabile contenga pid (ultimo elemento o primo senza ulteriori)
-  if (strstr(getenv("BPID"), convertIntToString(pid)) != NULL){
-    return strremove(getenv("BPID"), convertIntToString(pid));
-  }
-  return;
-}
-char * insertPid(int pid)
-{
-  int size = strlen(getenv("BPID"));
-  if(size==0)
-    size=100;
-  char myenv[size] = getenv("BPID");
-  if (getNumberOfChar(myenv,",")>0){
-    myenv = strcat(myenv, ",");
-        setenv("BPID", )
-  }
-  //char myenv[100] = getenv("BPID");
-}
-
-int getNumberOfChar(char * str,char c){
-  int ret =0;
-  for (int i = 0; i<strlen(str);i++){
-    if (str[i] == c)
-      ret++;
-  }
-  return ret;
-}
-char *strremove(char *str, const char *sub)
-{
-  size_t len = strlen(sub);
-  if (len > 0)
-  {
-    char *p = str;
-    while ((p = strstr(p, sub)) != NULL)
+    //controllo che la variabile contenga pid+,
+    char *str = convertIntToString(pid);
+    char *strWithComma = convertIntToString(pid);
+    strcat(strWithComma, ",");
+    if (strstr(getenv("BPID"), strWithComma) != NULL)
     {
-      memmove(p, p + len, strlen(p + len) + 1);
+      char *strRemoved = strremove(getenv("BPID"), strWithComma);
+      setenv("BPID", strRemoved, 1);
+    }
+    //controllo che la variabile contenga pid (ultimo elemento o primo senza ulteriori)
+    if (strstr(getenv("BPID"), str) != NULL)
+    {
+      setenv("BPID", strremove(getenv("BPID"), str), 1);
+    }
+    return;
+  }
+  void insertPid(int pid)
+  {
+    printf("insert pid %d", pid);
+    int size = 100;
+    char *env = getenv("BPID");
+    if (env != NULL)
+    {
+      if (strlen(env > 0))
+      {
+        size = strlen(getenv("BPID"));
+      }
+    }
+    printf("size is %d", size);
+    char myenv[size];
+    if (getenv("BPID") == NULL)
+    {
+      myenv[size] = "";
+    }
+    else
+    {
+      myenv[size] = getenv("BPID");
+    }
+
+    if (getNumberOfComma(myenv) > 0 || strlen(getenv("BPID")) > 0)
+    {
+      myenv[size] = strcat(myenv, ",");
+      myenv[size] = strcat(myenv, convertIntToString(pid));
+      setenv("BPID", myenv, 1);
+    }
+    else
+    {
+      myenv[size] = strcat(myenv, convertIntToString(pid));
+      setenv("BPID", myenv, 1);
     }
   }
-  return str;
-}
 
-char * convertIntToString(int n){
-  char str[12];
-  sprintf(str, "%d", n);
-  return &str;
-}
+  int getNumberOfComma(char str[])
+  {
+    int ret = 0;
+    for (int i = 0; i < strlen(str); i++)
+    {
+      if (str[i] == ',')
+        ret++;
+    }
+    return ret;
+  }
+  char *strremove(char *str, char *sub)
+  {
+    size_t len = strlen(sub);
+    if (len > 0)
+    {
+      char *p = str;
+      while ((p = strstr(p, sub)) != NULL)
+      {
+        memmove(p, p + len, strlen(p + len) + 1);
+      }
+    }
+    return str;
+  }
 
-int procline(void) /* tratta una riga di input */
-{
-  char *arg[MAXARG+1];	/* array di puntatori per runcommand */
-  int toktype;  	/* tipo del simbolo nel comando */
-  int narg;		/* numero di argomenti considerati finora */
-  int type;		/* FOREGROUND o BACKGROUND */
-	
-  narg=0;
+  char *convertIntToString(int n)
+  {
+    char str[12];
+    sprintf(str, "%d", n);
+    return &str;
+  }
 
-  while (1) {	/* ciclo da cui si esce con il return */
-	
-  /* esegue un'azione a seconda del tipo di simbolo */
-	
-    /* mette un simbolo in arg[narg] */
+  int procline(void) /* tratta una riga di input */
+  {
+    char *arg[MAXARG + 1]; /* array di puntatori per runcommand */
+    int toktype;           /* tipo del simbolo nel comando */
+    int narg;              /* numero di argomenti considerati finora */
+    int type;              /* FOREGROUND o BACKGROUND */
 
-    switch (toktype = gettok(&arg[narg])) {
+    narg = 0;
+
+    while (1)
+    { /* ciclo da cui si esce con il return */
+
+      /* esegue un'azione a seconda del tipo di simbolo */
+
+      /* mette un simbolo in arg[narg] */
+
+      switch (toktype = gettok(&arg[narg]))
+      {
 	
       /* se argomento: passa al prossimo simbolo */
       case ARG:
@@ -109,9 +145,9 @@ int procline(void) /* tratta una riga di input */
         type = (toktype == AMPERSAND) ? BACKGROUND : FOREGROUND;
       
         if (narg != 0) {
-	  arg[narg] = NULL;
+	        arg[narg] = NULL;
     
-    runcommand(arg,type);
+          runcommand(arg,type);
         }
       
 	/* se fine riga, procline e' finita */
@@ -131,8 +167,7 @@ int procline(void) /* tratta una riga di input */
 void runcommand(char **cline,int where)	/* esegue un comando */
 {
 
-  
-
+  fflush(stdout);
   pid = fork();
   if (pid == (pid_t)-1)
   {
@@ -149,6 +184,8 @@ void runcommand(char **cline,int where)	/* esegue un comando */
         pid_t p;
         int exitstat, ret;
 
+        fflush(stdout);
+
         p = fork();
         if (p == -1)
         {
@@ -158,7 +195,8 @@ void runcommand(char **cline,int where)	/* esegue un comando */
 
         if (p == 0)
         { /* processo figlio */
-          setenv("BPID",convertIntToString(p),1);
+          //setenv("BPID",convertIntToString(p),1);
+          //insertPid(p);
           execvp(*cline, cline);
           exit(EXIT_SUCCESS);
         }else{
@@ -175,12 +213,16 @@ void runcommand(char **cline,int where)	/* esegue un comando */
             else if (endID == p)
             { /* child ended                 */
               if (WIFEXITED(status)){
-                printf("The command %s is terminated ", *cline);
+                printf("The command %s is terminated \n", *cline);
               }
               else if (WIFSIGNALED(status))
-                printf("Child ended because of an uncaught signal.n");
+                printf("Child ended because of an uncaught signal.n \n");
               else if (WIFSTOPPED(status))
-                printf("Child process has stopped.n");
+                printf("Child process has stopped.n \n");
+              else
+                printf("Child");
+              //removePid()
+              fflush(stdout);
               exit(EXIT_SUCCESS);
             }
           }
@@ -239,10 +281,11 @@ char* getUserNameAndWorkingDir()
 
 int main()
 {
-  
-      prompt = getUserNameAndWorkingDir();
+  setenv("BPID","",1);
+  prompt = getUserNameAndWorkingDir();
   while (userin(prompt) != EOF){
     //to fix prompt after first init
+    fflush(stdout);
     prompt = getUserNameAndWorkingDir();
     procline();
   }
